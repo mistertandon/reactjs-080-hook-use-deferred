1fef1e54c3f7b9b3513400f5a0c9921d60f5608a
mistertandon <enggparveshtandon@gmail.com>
2017-04-21 16:45:54 +0530

    Implemented basic AngularJS filter functionality.

--- app/filter/filter-home.js
+++ app/filter/filter-home.js
@@ -1,11 +1,53 @@
 (function (angular) {
   'use strict';
 
-  var FilterHomeControllerFn = function () {
+  var FilterHomeControllerFn = function ($filter) {
 
     var self = this;
 
     self.title = "AngularJS Filter";
+
+    /**
+     * `candidatesList` contains candidates list.
+     */
+    self.candidatesList = self.candidatesListRef = [
+      { personId: 10, firstName: "John", lastName: "Smith", Sex: "Male" },
+      { personId: 11, firstName: "James", lastName: "Last", Sex: "Male" },
+      { personId: 12, firstName: "Mary", lastName: "Heart", Sex: "Female" },
+      { personId: 13, firstName: "Sandra", lastName: "Goldsmith", Sex: "Female" },
+      { personId: 14, firstName: "Shaun", lastName: "Sheep", Sex: "Male" },
+      { personId: 15, firstName: "Nicola", lastName: "Smith", Sex: "Male" }
+    ];
+
+    /**
+     * This function is used to apply filter on candidatesList on the basis of 'Sex'
+     */
+    self.filterCandidateBySex = function (sex) {
+
+      self.candidatesList = $filter('CandidateFilteriationBySex')(self.candidatesListRef, sex);
+    }
+
+    /**
+     * This function is used to reset `CandidateFilteriationBySex` filter.
+     */
+    self.filterCandidateReset = function () {
+
+      self.candidatesList = self.candidatesListRef;
+    }
+  }
+
+  /**
+   * This function contains definition for `CandidateFilteriationBySex` filter.
+   */
+  var CandidateFilteriationBySexFn = function () {
+
+    return function (candidateList, sex) {
+
+      return _.filter(candidateList, function (o) {
+
+        return o.Sex == sex;
+      });
+    }
   }
 
   angular
@@ -27,6 +69,8 @@
 
     }])
 
-    .controller('FilterHomeController', [FilterHomeControllerFn]);
+    .controller('FilterHomeController', ["$filter", FilterHomeControllerFn])
+
+    .filter('CandidateFilteriationBySex', CandidateFilteriationBySexFn);
 
 })(window.angular);
